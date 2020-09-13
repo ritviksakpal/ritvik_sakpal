deac9f8952f9a16dda5c7f26114dc4b398dd950c
ritviksakpal <47916596+ritviksakpal@users.noreply.github.com>
2020-09-13 23:55:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".submit").click(function() {
+        // event.preventDefault()
+        console.log("clicked button")
+
+        var fname = $(".fname").val()
+        var lname = $(".lname").val()
+        var phone = $(".phone").val()
+        var email = $(".email").val()
+        var message = $(".message").val()
+
+        var status = $(".status")
+
+
+    })
+})
